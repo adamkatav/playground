bcee7404c201d39a213a76f73080a3478cec7e75
Adam Katav <adamkatav@gmail.com>
2021-11-08 23:27:27 +0200

    Made "typedef struct complex* Complex"

--- complex.c
+++ complex.c
@@ -3,35 +3,42 @@
 #include <string.h>
 #include <stdio.h>
 
+struct complex{
+    int real;
+    int imaginary;
+};
 
-Complex CreateComplex(int real, int imaginary){
-    Complex a = {real, imaginary};
+Complex ComplexCreate(int real, int imaginary){
+    Complex a = malloc(sizeof(struct complex));
+    a->real = real;
+    a->imaginary = imaginary;
     return a;
 }
+void ComplexDelete(Complex z){
+    free(z);
+}
 
-int Re(Complex* z){
+int Re(Complex z){
     return z->real;
 }
 
-int Im(Complex* z){
+int Im(Complex z){
     return z->imaginary;
 }
 
-Complex ComplexAdd(Complex* z1, Complex* z2){
-    Complex a = {Re(z1) + Re(z2), Im(z1) + Im(z2)};
-    return a;
+Complex ComplexAdd(Complex z1, Complex z2){
+    return CreateComplex(Re(z1) + Re(z2),Im(z1) + Im(z2));
 }
 
-Complex ComplexMul(Complex* z1, Complex* z2){ // (a + ib) * (c + id) = ac +iad + ibc + -(db) = (ac - db) + i(ad + bc)
-    Complex a = {Re(z1)*Re(z2) - Im(z1)*Im(z2), Re(z1)*Im(z2) + Im(z1)*Re(z2)};
-    return a;
+Complex ComplexMul(Complex z1, Complex z2){ // (a + ib) * (c + id) = ac +iad + ibc + -(db) = (ac - db) + i(ad + bc)
+    return CreateComplex(Re(z1)*Re(z2) - Im(z1)*Im(z2),Re(z1)*Im(z2) + Im(z1)*Re(z2));
 }
 
-int ComplexAbs(Complex* z){
+int ComplexAbs(Complex z){
     return sqrt( pow(Re(z),2) + pow(Im(z),2) );
 }
 
-void ToString(Complex* z, char* str /*Assumed to be at least 21 long and null terminated*/){
+void ToString(Complex z, char* str /*Assumed to be at least 21 long and null terminated*/){
     sprintf(str,"%d + i%d",Re(z), Im(z));
 }
 
--- complex.h
+++ complex.h
@@ -1,14 +1,14 @@
 #ifndef COMPLEX_H
 #define COMPLEX_H
-typedef struct Complex{
-    int real;
-    int imaginary;
-} Complex;
-Complex CreateComplex(int real, int imaginary);
-int Re(Complex* z);
-int Im(Complex* z);
-Complex ComplexAdd(Complex* z1, Complex* z2);
-Complex ComplexMul(Complex* z1, Complex* z2);
-int ComplexAbs(Complex* z);
-void ToString(Complex* z, char* str /*Assumed to be at least 21 long and null terminated*/);
+
+typedef struct complex* Complex;
+
+Complex ComplexCreate(int real, int imaginary);
+void ComplexDelete(Complex z);
+int Re(Complex z);
+int Im(Complex z);
+Complex ComplexAdd(Complex z1, Complex z2);
+Complex ComplexMul(Complex z1, Complex z2);
+int ComplexAbs(Complex z);
+void ToString(Complex z, char* str /*Assumed to be at least 21 long and null terminated*/);
 #endif
